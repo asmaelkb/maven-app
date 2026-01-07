40193ea0cc7d58fb110c5a3cbc764150f0048953
Asmae <91834907+asmaelkb@users.noreply.github.com>
2026-01-07 15:16:00 +0000

    re-test

--- src/test/java/com/example/calculator/CalculatorParamTest.java
+++ src/test/java/com/example/calculator/CalculatorParamTest.java
@@ -4,7 +4,7 @@ package com.example.calculator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorParamTest {
 
